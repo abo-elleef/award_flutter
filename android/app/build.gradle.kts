95d1df0753ac8bbf800eabcf3fe57ea2072ce4d1
abo-elleef <a.aboelllef@gmail.com>
2025-09-14 13:02:50 +0300

    release Madeeh

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.leef.awrad"
+    namespace = "com.leef.elborda"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.13599879"
 
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.leef.awrad"
+        applicationId = "com.leef.elborda"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
